af10e2352b180eb87cace38f08066e4041f12ae2
Md. Ashraful Islam <mdashrafulpramanic@gmail.com>
2024-11-08 21:36:23 +0600

    Code from Ashraful part is added

---
+++ usecase-to-testcase-mai/README.md
@@ -0,0 +1,4 @@
+dataset.ipynb - Create json dataset from collected data \
+gemini_rag.ipynb - Create vector database embedding the collected json dataset, to be used for RAG retrieval \
+retrieval.py - to retrieve best matches from the vector db for a particular query \
+generate_test_case.ipynb - directly generate test case from a use case, uses RAG approach to add 3 examples to the prompt \
---
+++ usecase-to-testcase-mai/retrieve.py
@@ -0,0 +1,52 @@
+import google.generativeai as genai
+from chromadb import Documents, EmbeddingFunction, Embeddings
+import os
+import chromadb
+from typing import List
+
+class GeminiEmbeddingFunction(EmbeddingFunction):
+    """
+    Custom embedding function using the Gemini AI API for document retrieval.
+
+    This class extends the EmbeddingFunction class and implements the __call__ method
+    to generate embeddings for a given set of documents using the Gemini AI API.
+
+    Parameters:
+    - input (Documents): A collection of documents to be embedded.
+
+    Returns:
+    - Embeddings: Embeddings generated for the input documents.
+    """
+    def __call__(self, input: Documents) -> Embeddings:
+        gemini_api_key = "AIzaSyCYZFFO_Yr8C62LU2_HxGbOFZSYNEZKHi4"
+        if not gemini_api_key:
+            raise ValueError("Gemini API Key not provided. Please provide GEMINI_API_KEY as an environment variable")
+        genai.configure(api_key=gemini_api_key)
+        model = "models/embedding-001"
+        title = "Custom query"
+        return genai.embed_content(model=model,
+                                   content=input,
+                                   task_type="retrieval_document",
+                                   title=title)["embedding"]
+
+
+def load_chroma_collection(path, name):
+    """
+    Loads an existing Chroma collection from the specified path with the given name.
+
+    Parameters:
+    - path (str): The path where the Chroma database is stored.
+    - name (str): The name of the collection within the Chroma database.
+
+    Returns:
+    - chromadb.Collection: The loaded Chroma Collection.
+    """
+    chroma_client = chromadb.PersistentClient(path=path)
+    db = chroma_client.get_collection(name=name, embedding_function=GeminiEmbeddingFunction())
+
+    return db
+
+
+def get_relevant_passage(query, db, n_results):
+  passage = db.query(query_texts=[query], n_results=n_results)['documents'][0]
+  return passage
